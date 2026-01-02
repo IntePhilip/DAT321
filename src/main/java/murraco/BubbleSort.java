86a481242b5e966dd6cb59a4fd6dd4e3094eb701
Erik <erikpersson0884@gmail.com>
2026-01-02 13:39:15 +0100

    introduced a fault in bubblesort

--- src/main/java/murraco/BubbleSort.java
+++ src/main/java/murraco/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
 
   // Time complexity: average O(n^2) and best O(n) - Space complexity: O(1)
   public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
+    for (int i = 0; i < arr.length - 2; i++) {
       for (int j = 1; j < arr.length; j++) {
         if (arr[j].compareTo(arr[j - 1]) < 0) {
           T temp = arr[j];
